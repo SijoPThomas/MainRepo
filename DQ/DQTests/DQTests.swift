7728ce6988a5147d9f0463458092aadb2c8393e4
Sijo P Thomas <sijopthomas@MacBook-Air.local>
2016-11-10 20:31:54 +0530

    Initial Commit

---
+++ DQ/DQTests/DQTests.swift
@@ -0,0 +1,36 @@
+//
+//  DQTests.swift
+//  DQTests
+//
+//  Created by Sijo P Thomas on 10/11/16.
+//  Copyright © 2016 PRS. All rights reserved.
+//
+
+import XCTest
+@testable import DQ
+
+class DQTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
